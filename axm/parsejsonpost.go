9401366667749315edb8253b63411547b359f5ff
barglee <barglee83@gmail.com>
2022-05-27 16:03:01 +0100

    fix free mem to use %

--- axm/parsejsonpost.go
+++ axm/parsejsonpost.go
@@ -109,6 +109,9 @@ func writeToInflux(lmc LoadMaster) {
     writeAPI.WriteRecord(fmt.Sprintf("lmcpu,Custid="+strconv.Itoa(lmc.Custid)+",Lmclusterid=1,Name=Xname User="+strconv.Itoa(lm.Cpu.User)+",System="+strconv.Itoa(lm.Cpu.System)+",Idle="+strconv.Itoa(lm.Cpu.Idle)+",Iowait="+strconv.Itoa(lm.Cpu.Iowait)))
     writeAPI.WriteRecord(fmt.Sprintf("lmmem,Custid="+strconv.Itoa(lmc.Custid)+",Lmclusterid=1,Name=Xname Used="+strconv.Itoa(lm.Memory.Percentmemused)+",Free="+strconv.Itoa(lm.Memory.Memfree)+",Memused="+strconv.Itoa(lm.Memory.Memused)))
     writeAPI.WriteRecord(fmt.Sprintf("lmfw,Custid="+strconv.Itoa(lmc.Custid)+",Lmclusterid=1,Name=Xname Firmware=\""+lm.Firmware+"\""))
+    writeAPI.WriteRecord(fmt.Sprintf("lmsubs,Custid="+strconv.Itoa(lmc.Custid)+",Lmclusterid=1,Name=Xname Substier=\""+lm.License.Substier+"\",Subsexpiry=\""+lm.License.Subsexpiry+"\",Subsexpirydays=\""+strconv.Itoa(lm.License.Subsexpirydays)+"\""))
+
+
     writeAPI.WriteRecord(fmt.Sprintf("lmmodel,Custid="+strconv.Itoa(lmc.Custid)+",Lmclusterid=1,Name=Xname Model=\""+lm.Model+"\""))
     writeAPI.WriteRecord(fmt.Sprintf("lmip,Custid="+strconv.Itoa(lmc.Custid)+",Lmclusterid=1,Name=Xname Ip=\""+lm.IP+"\""))
 
