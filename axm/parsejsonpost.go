3045eddc9e67ba65d9cfd2f94053b13679e62daa
barglee <barglee83@gmail.com>
2022-05-27 12:54:51 +0100

    fix free mem to use %

--- axm/parsejsonpost.go
+++ axm/parsejsonpost.go
@@ -29,7 +29,7 @@ type LoadMaster struct {
 		IP   string `json:"ip"`
 		Mode string `json:"mode"`
 		Model string `json:"model"`
-		//Firmware string `json:"firmware"`
+		Firmware string `json:"firmware"`
 		Cpu  struct{
 		    User          int `json:"user"`
 		    System        int `json:"system"`
